86cad5cb340ba3e6d83346bc47b142504a02b007
AlexLunarr <102823767+AlexLunarr@users.noreply.github.com>
2023-08-06 22:42:51 +0300

    setting_project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 
 import './index.html';
 import './index.scss';
-import code from './img/slider-pic5_1.png' ;
-import { mult, sum } from './modules/calc';
+import './modules/modules';
 
 
+import headerContent from './partials/header.html';
+document.getElementById('header-content').innerHTML = headerContent;
+
+import mainContent from './partials/main.html';
+document.getElementById('main-content').innerHTML = mainContent;
+
+import footerContent from './partials/footer.html';
+document.getElementById('footer-content').innerHTML = footerContent;
+
+import heroContent from './partials/components/hero.html';
+document.getElementById('hero').innerHTML = heroContent;
+
+import aboutContent from './partials/components/about.html';
+document.getElementById('about').innerHTML = aboutContent;
+
+import tasksContent from './partials/components/tasks.html';
+document.getElementById('tasks').innerHTML = tasksContent;
+
+import projectsContent from './partials/components/projects.html';
+document.getElementById('projects').innerHTML = projectsContent;
 
 
-const imgWrap = document.querySelector('.image');
-const img = new Image();
-img.src = code;
-img.width = 700
-imgWrap.append(img)
+import feedbackContent from './partials/components/feedback.html';
+document.getElementById('feedback').innerHTML = feedbackContent;
 
-
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+import mobileMenu from './partials/components/mobile_menu.html';
+document.getElementById('mobile_menu').innerHTML = mobileMenu;
